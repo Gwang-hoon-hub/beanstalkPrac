b323be6fed7c537d36ed99465220a26b48c93e70
panghoon <leegwanghoon95@gmail.com>
2022-03-25 17:23:44 +0900

    Practice : beanstalk + log trace

---
+++ src/main/java/com/pang/beanstalkprac/BeanstalkPracApplication.java
@@ -0,0 +1,13 @@
+package com.pang.beanstalkprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanstalkPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeanstalkPracApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pang/beanstalkprac/BeanstalkPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pang.beanstalkprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanstalkPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
